fe286c73a4cd01071d52e3b11855898d7043e826
DragonOfWar <therealdragonofwar@gmail.com>
2022-03-02 21:14:22 -0300

    Use kate global variable to select default text editor

--- editenc.sh
+++ editenc.sh
@@ -5,12 +5,15 @@ if [ ! -f $1 ]; then
     exit 1
 fi
 
-DEFAULT_EDITOR="kate"
+if [ -z $EDITOR ]; then 
+    echo "$0 warning: No text editor has been set to \$EDITOR. Using nano instead. To suppress this warning, add 'export \$EDITOR=\"your-preferred-text-editor\"' to your .bashrc file."
+    EDITOR="nano"
+fi
 
 echo -n "Encrypted file password: "
 read -s PW
 echo $PW | gpg --batch --passphrase-fd 0 --output /tmp/unencryptedfiletoedit -d $1
 if [ $? -ne 0 ]; then exit $?; fi
-$DEFAULT_EDITOR /tmp/unencryptedfiletoedit
+$EDITOR /tmp/unencryptedfiletoedit
 echo $PW | gpg --batch --yes --passphrase-fd 0 --output $1 -c /tmp/unencryptedfiletoedit
 rm /tmp/unencryptedfiletoedit
